56d86aee8414ac21c9a096ed44e9c7bca28de145
Aicha-Dennoune <aicha.dennoune06@gmail.com>
2024-12-10 00:26:29 +0100

    Update EmployeeDAOI.java

--- src/DAO/EmployeeDAOI.java
+++ src/DAO/EmployeeDAOI.java
@@ -4,9 +4,9 @@ import Model.Employee;
 import java.util.List;
 
 public interface EmployeeDAOI {
-    void add(Employee employee); // Ajouter un employé
-    void delete(int id); // Supprimer un employé
-    List<Employee> listAll(); // Lister tous les employés
-    Employee findById(int id); // Trouver un employé par ID
-    void update(Employee employee, int id); // Mettre à jour un employé
+    void add(Employee employee); 
+    void delete(int id); 
+    List<Employee> listAll(); 
+    Employee findById(int id);D
+    void update(Employee employee, int id);
 }
