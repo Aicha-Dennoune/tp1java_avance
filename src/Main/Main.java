32c3aa7becd0a245fbe0eda7899d2f34c65d76bd
Aicha-Dennoune <aicha.dennoune06@gmail.com>
2024-12-20 01:18:24 +0100

    Update Main.java

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,12 +1,28 @@
 package Main;
 
 import Controller.EmployeeController;
+import Controller.HolidayController;
 import View.EmployeeView;
+import View.HolidayView;
 
 public class Main {
     public static void main(String[] args) {
-        EmployeeView view = new EmployeeView();
-        new EmployeeController(view);
-        view.setVisible(true);
+        EmployeeView employeeView = new EmployeeView();
+        HolidayView holidayView = new HolidayView();
+
+        new EmployeeController(employeeView, holidayView);
+        new HolidayController(holidayView);
+
+        employeeView.setVisible(true);
+
+        employeeView.switchViewButton.addActionListener(e -> {
+            employeeView.setVisible(false);
+            holidayView.setVisible(true);
+        });
+
+        holidayView.switchViewButton.addActionListener(e -> {
+            holidayView.setVisible(false);
+            employeeView.setVisible(true);
+        });
     }
 }
